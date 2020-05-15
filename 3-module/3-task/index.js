726276b6b7f8ada186f3bba4758b58011201dbaa
Ivan Zabaznov <ivan.zabaznov@gmail.com>
2020-05-15 03:07:24 +0300

    Написал функцию camelize.

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -3,4 +3,9 @@
  * @returns {string}
  */
 function camelize(str) {
+  return str
+    .split('-')
+    .reduce((newStr, str, index) => {
+      return index === 0 ? newStr = str : newStr += str[0].toUpperCase() + str.slice(1);
+    }, '');
 }
