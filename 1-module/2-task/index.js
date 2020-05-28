a287bccd351071b3f888aff9b9c57f2dc5e86fc6
Ivan Zabaznov <ivan.zabaznov@gmail.com>
2020-05-28 16:38:07 +0300

    Поправил функцию isValid.

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -10,7 +10,7 @@ function print(text) {
  * чтобы функция sayHello работала корректно
  */
 function isValid(name) {
-  if (name === undefined || name.includes(' ') || name.length < 4) return false;
+  if (!name || name.includes(' ') || name.length < 4) return false;
 
   return true;
 }
